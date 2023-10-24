caecb5fce2bad675d198bfaa43fd227f2bb39812
Aditya Jindal <raghavjindal017@gmail.com>
2023-10-24 21:53:32 +0530

    socials added

--- dist/script.js
+++ dist/script.js
@@ -6,6 +6,7 @@ import {
 const n1 = document.querySelector("#xoogler");
 const wiz = document.querySelector("#wizards");
 const n2 = document.querySelector("#google");
+const n3 = document.querySelector("#uf");
 const simple = document.querySelector("#simple")
 const n4 = document.querySelector("#del");
 const n6 = document.querySelector("#tech2");
@@ -18,6 +19,7 @@ const n12 = document.querySelector("#intern");
 
 const a1 = annotate(n1, { type: "highlight", color: "#f8c9e3" });
 const a2 = annotate(n2, { type: "underline", color: "green", padding: 1 });
+const a3 = annotate(n3, { type: "highlight", color: "#f9c5c0" });
 const aWiz = annotate(wiz, { type: "underline", color: "#0c3475", padding: 1 });
 const a4 = annotate(n4, { type: "highlight", color: "#fce37c" });
 const a6 = annotate(n6, { type: "highlight", color: "#e5cefc" });
@@ -35,6 +37,7 @@ aWiz.show();
 
 const ag2 = annotationGroup([a6, a7, a8, a9, a10]);
 aSimple.show();
+a3.show();
 a4.show();
 
 ag2.show();
